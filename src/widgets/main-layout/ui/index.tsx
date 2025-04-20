2fc0f028af49006841b48f671a0353ac773555d1
Ravshan Davlatov <davlatovravshan014@gmail.com>
2025-04-20 05:47:47 +0500

    Update index.tsx

--- src/widgets/main-layout/ui/index.tsx
+++ src/widgets/main-layout/ui/index.tsx
@@ -1,31 +1,24 @@
 import React, {useEffect} from 'react';
 import styles from './styles.module.scss';
-import {Navbar} from "../../navbar";
 import {Outlet} from "react-router-dom";
 import {useAppStore} from "../../../shared/model/app-store.ts";
 import {UI_COLOR} from "../../../shared/consts.ts";
-import {Spinner} from "@chakra-ui/react";
 
 export const MainLayout = () => {
 
     const isAppLoading = useAppStore(state => state.isAppLoading)
-    const initTelegram = useAppStore(state => state.initTelegram)
-
-    useEffect(() => {
-        initTelegram();
-    }, [])
 
     if (isAppLoading) {
         return (
             <div className='AppLoader'>
-                <Spinner color={UI_COLOR} size='xl'/>
+                Loading...
             </div>
         )
     }
 
     return (
         <div className={styles.wrapper}>
-            <Navbar/>
+            {/* Navbar */}
             <div className={styles.content}>
                 <Outlet/>
             </div>
